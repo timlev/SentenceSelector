529d3f31fa7216123301f159728ed9f59782afc0
Tim Leverentz <timlev@gmail.com>
2017-10-11 22:29:31 -0500

    Initial Commit

---
+++ paragraph.js
@@ -0,0 +1,4 @@
+var paragraph = [{"The toilet is a useful invention." : "m"},
+{"The toilet helps prevent the spread of disease." : "d"},
+{"The toilet allows many people to live in one place." : "d"},
+{"The toilet is a beautiful throne that makes you feel like royalty." : "d"}]
