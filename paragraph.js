8f7795f5a2cc519cecd796682d4f7ed0f871657b
Tim Leverentz <timlev@gmail.com>
2017-10-13 17:17:28 -0500

    Working on making btn text clickable

--- paragraph.js
+++ paragraph.js
@@ -1,4 +1,4 @@
 var paragraph = [{"Vegetables are good for you." : "m"},
 {"Vegetables help your immune system." : "d"},
 {"Vegetables help your digestive system work properly." : "d"},
-{"Vegetables help your whole body function." : "d"}]
+{"Vegetables help your whole body function." : "d"}];
