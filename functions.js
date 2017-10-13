70a1f03dfb9b6e02eaa36993fa3415540a256379
Tim Leverentz <timlev@gmail.com>
2017-10-13 17:41:49 -0500

    Working on making btn text clickable

--- functions.js
+++ functions.js
@@ -129,6 +129,6 @@ window.onload = function (){
 	};
 	var mainideabtn = document.getElementById("mainideabtn");
 	var detailsbtn = document.getElementById("detailsbtn");
-	mainideabtn.addEventListener("click",function(){console.log(document.forms[0][0].value);});
+	mainideabtn.addEventListener("click",function(){document.getElementById("status").innerText = document.forms[0][0].value;});
 	detailsbtn.addEventListener("click",function() {alert("Clicked Details");});
 };
