9f877e595c5866f9f690ce1b494f8548b8cc4a74
Tim Leverentz <timlev@gmail.com>
2017-10-13 23:48:41 -0500

    Added fonts

---
+++ moreparagraphs.js
@@ -0,0 +1,22 @@
+var paragraph = [{"Minnesota United FC is an American professional soccer club based in the Minneapolis-Saint Paul area." : "m"},
+{"MN United plays in the Western Conference of Major League Soccer." : "d"},
+{"The club began play in 2017 as the league's 22nd club." : "d"},
+{"The club currently plays its home games at TCF Bank Stadium, on the campus of the University of Minnesota in Minneapolis." : "d"},
+{"The club eventually plans to play in Saint Paul (becoming the city's second major professional sports team after the Minnesota Wild), at Allianz Field in the Midway neighborhood." : "d"},
+{"The mascot of the club is a loon.": "d"}]
+
+var paragraph = [{"The Minnesota Lynx are a professional basketball team based in Minneapolis, Minnesota." : "m"},
+{"They play in the Women's National Basketball Association (WNBA)." : "d"},
+{"The team was created before the 1999 season." : "d"},
+{"The Lynx have made it to the playoffs in nine of their nineteen years in Minnesota." : "d"},
+{"The team won the WNBA championship in 2011, 2013, 2015, and 2017." : "d"},
+{"The team has had many high-quality players such as Katie Smith, explosive small-forward Seimone Augustus, native Minnesotan Lindsay Whalen, and Connecticut standout Maya Moore." : "d"]
+
+var paragraph = [{"St.Anthony Village High School is a public high school with many successful teams located in St. Anthony, Minnesota." : "d"},
+{"St. Anthony’s team mascot is the Huskie." : "d"},
+{"The team won the 2006 and 2008 State AA baseball championships." : "d"},
+{"The St. Anthony's RoboHuskie team has participated in the FIRST Robotics program since the 2007-2008 season." : "d"},
+{"In 2010 they won the Wisconsin regional competition." : "d"},
+{"The school is not large enough to support all sports on its own, so boys’ hockey is shared with Irondale High School and girls’ hockey, co-ed Nordic skiing, wrestling, and co-ed track are shared with Spring Lake Park High School." : "d"},
+{"Men and women soccer is combined with Spring Lake Park High School." : "d"},
+{"In the last eight years St. Anthony men's soccer team has reached six section finals!" : "d"}]
--- paragraph.js
+++ paragraph.js
@@ -1,4 +1,6 @@
-var paragraph = [{"Vegetables are good for you." : "m"},
-{"Vegetables help your immune system." : "d"},
-{"Vegetables help your digestive system work properly." : "d"},
-{"Vegetables help your whole body function." : "d"}];
+var paragraph = [{"Minnesota United FC is an American professional soccer club based in the Minneapolis-Saint Paul area." : "m"},
+{"MN United plays in the Western Conference of Major League Soccer." : "d"},
+{"The club began play in 2017 as the league's 22nd club." : "d"},
+{"The club currently plays its home games at TCF Bank Stadium, on the campus of the University of Minnesota in Minneapolis." : "d"},
+{"The club eventually plans to play in Saint Paul (becoming the city's second major professional sports team after the Minnesota Wild), at Allianz Field in the Midway neighborhood." : "d"},
+{"The mascot of the club is a loon.": "d"}]
